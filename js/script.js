23615d28e67f9cbbd1d51a5d51e11f0c7151d580
test <guadalupereyra@outlook.com>
2020-01-13 20:53:41 -0300

    Agrego generador de ID

--- js/script.js
+++ js/script.js
@@ -1,32 +1,47 @@
-
-
 //Debo crear la clase nota con todos sus atributos
-class Nota{
-//Constructor
-    constructor(id, titulo, contenido, fecha){
-        this.id=id;
-        this.titulo=titulo;
-        this.contenido=contenido;
-        this.fecha=fecha;
+class Nota {
+  //Constructor
+  constructor(id, titulo, contenido, fecha) {
+    this.id = id;
+    this.titulo = titulo;
+    this.contenido = contenido;
+    this.fecha = fecha;
+  }
+  //setters y getters
+  setTitulo(titulo) {
+    this.titulo = titulo;
+  }
+  getTitulo() {
+    return this.titulo;
+  }
 
-    }
-//setters y getters
-    setTitulo(titulo){
-        this.titulo=titulo;
-    }
-    getTitulo(){
-        return this.titulo;
-    }
-    
-    setContenido(contenido){
-        this.contenido=contenido;
-    }
-    getContenido(){
-        return this.contenido;
-    }
+  setContenido(contenido) {
+    this.contenido = contenido;
+  }
+  getContenido() {
+    return this.contenido;
+  }
+}
 
+//Metodo de generacion random de ID
 
-    //metodo agregar nota
+function generarId(length) {
+  let idBox = document.getElementById("inpId");
+  var id = "";
+  var caracteres =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var caracteresLength = caracteres.length;
+  for (var i = 0; i < length; i++) {
+    id += caracteres.charAt(Math.floor(Math.random() * caracteresLength));
+  }
 
-    
+  idBox.value = id;
+}
+
+//Metodo de agregar nota
+
+function agregarNota() {
+  id;
+  titulo;
+  contenido;
 }
