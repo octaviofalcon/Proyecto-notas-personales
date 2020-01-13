c5ecfee9072b126f8d9f3a22fd6de9f1c3f22746
test <guadalupereyra@outlook.com>
2020-01-13 20:43:34 -0300

    agrego clase nota con constructor

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+//Debo crear la clase nota con todos sus atributos
+class Nota{
+//Constructor
+    constructor(id, titulo, contenido, fecha){
+        this.id=id;
+        this.titulo=titulo;
+        this.contenido=contenido;
+        this.fecha=fecha;
+
+    }
+
+
+}
